e0c04abaef62437ea582c3de24952af846704ee8
TANVIRALAMKHAN72 <tanviralamkhantonmoy@gmail.com>
2025-06-17 20:26:34 +0600

    first project

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -5,8 +5,6 @@ import errorLottie2 from '../assets/error2.json';
 import Lottie from 'lottie-react';
 
 
-
-
 const Error = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-red-50 to-orange-100 dark:from-gray-900 dark:to-gray-800 text-center px-6">
