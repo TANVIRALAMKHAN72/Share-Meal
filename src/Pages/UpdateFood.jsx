fe36c8912dab63f8ebc47eee84330ee36e0d175a
TANVIRALAMKHAN72 <tanviralamkhantonmoy@gmail.com>
2025-08-09 14:37:00 +0600

    the first blog

--- src/Pages/UpdateFood.jsx
+++ src/Pages/UpdateFood.jsx
@@ -93,14 +93,12 @@ const UpdateFood = ({ id, isOpen, onClose }) => {
 
         <h2 className="text-3xl font-semibold mb-6 text-center">Update Food</h2>
 
-        
         {loading ? (
             <div className="min-h-screen flex justify-center items-center">
     <span className="loading loading-spinner loading-lg text-primary"></span>
   </div>
         ) : (
           <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            
             <div className="md:col-span-2">
               <label className="block mb-1 font-semibold">Food Image URL</label>
               <input
